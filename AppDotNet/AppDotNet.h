ad3bae4d0921c975215dec8a36072640b2563f04
Matt Rubin <mattrubin@me.com>
2013-01-22 00:18:19 -0800

    [minor] Client categories in library header

--- AppDotNet/AppDotNet.h
+++ AppDotNet/AppDotNet.h
@@ -8,6 +8,15 @@
 
 #import <AppDotNet/ADNClient.h>
 #import <AppDotNet/ADNClient+ADNUser.h>
+#import <AppDotNet/ADNClient+ADNPost.h>
+#import <AppDotNet/ADNClient+ADNChannel.h>
+#import <AppDotNet/ADNClient+ADNMessage.h>
+#import <AppDotNet/ADNClient+ADNStream.h>
+#import <AppDotNet/ADNClient+ADNFilter.h>
+#import <AppDotNet/ADNClient+ADNInteraction.h>
+#import <AppDotNet/ADNClient+ADNStreamMarker.h>
+#import <AppDotNet/ADNClient+ADNText.h>
+#import <AppDotNet/ADNClient+ADNToken.h>
 
 #import <AppDotNet/ADNAuthenticationRequest.h>
 
