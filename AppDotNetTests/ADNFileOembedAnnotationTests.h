f4d5ffd404361a919e20bc442eab758c484814d9
Thomas Westfeld <westfeld@mac.com>
2013-03-16 15:19:03 +0100

    Added fileoembed annotation
    Squashed commit of the following:
    
    commit fb2fa44c33a3c1da824afa7613a03ca2d5aa4d5a
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Sat Mar 16 14:48:27 2013 +0100
    
        Added test cases for FileOembedAnnotations
    
        * Finished implementation of ADNFileOembedAnnotationTests
        * Added tests to ensure the correct output
        * TODO: write the parser part, too
    
    commit 16b1f505ae7f620039e991ee87a93092eb256e73
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Sat Mar 16 12:17:56 2013 +0100
    
        adapted GeolocationTests to new category with helper methods
    
    commit de1e3a60eca8ab93cafe1bce7e8fa8bc0b59ff1f
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Sat Mar 16 12:15:18 2013 +0100
    
        refactored ADNModelTests helper methods into category
    
        the helper methods for recursive object comparison were refactored from
        ADNModelTests into its own category on SenTestCase. This facilitates to
        test more special models like annotations were additional functionality
        need testing.
    
    commit d635abe8385a24cb1ce1eb8b80506b670cd71403
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Fri Mar 15 22:54:08 2013 +0100
    
        added unit tests for geolocation annotation
    
        unit testing of ADNGeolocation
        * round trip to JSON and back
        * conversion to CLLocation object
        * creation from CLLocation object
        * setting attributes from CLLocation object on a existing ADNGeolocation object
    
    commit c72240d7966658e47f0eb5ad8f9c788eaa2777ca
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Fri Mar 15 21:39:10 2013 +0100
    
        added FileOembedAnnotation mock data
    
    commit 97973c26ae06fa28e86faed5842cc10bb6f2fa61
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Mon Mar 11 22:02:35 2013 +0100
    
        JSON serialization seems to start working
    
    commit c02eaba60f781987d4416db755cd14877a59b7ee
    Author: Thomas Westfeld <westfeld@mac.com>
    Date:   Mon Mar 11 20:50:19 2013 +0100
    
        started file oembed annotation subclass

---
+++ AppDotNetTests/ADNFileOembedAnnotationTests.h
@@ -0,0 +1,18 @@
+//
+//  ADNFileOembedAnnotationTests.h
+//  AppDotNet
+//
+//  Created by Thomas Westfeld on 16.03.13.
+//  Copyright (c) 2013 Matt Rubin. All rights reserved.
+//
+
+// tests the correct generation of FileOembedAnnotations
+// no round trip is checked because the +net.app.core.file key is processed by
+// the ADN API and something different is returned
+
+
+#import "ADNModelTests.h"
+
+@interface ADNFileOembedAnnotationTests : SenTestCase
+
+@end
